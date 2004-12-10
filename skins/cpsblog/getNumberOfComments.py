58db8c59afa53dbe819344e76cf6f3bf75f8effa
rspivak <devnull@localhost>
2004-12-10 19:00:17 +0000

    Modified to not use bare except and to use default argument of restrictedTraverse in case object is not found.

--- skins/cpsblog/getNumberOfComments.py
+++ skins/cpsblog/getNumberOfComments.py
@@ -9,9 +9,9 @@ This gives us total number of comments posts to proxy_blogentry.
 path = list(context.portal_url.getRelativeContentPath(proxy_blogentry))
 path.insert(-1, '.cps_discussions')
 path = '/'.join(path)
-try:
-    obj = context.restrictedTraverse(path)
-except:
+
+obj = context.restrictedTraverse(path, None)
+if obj is None:
     return 0
 else:
     return len(obj.objectIds())
