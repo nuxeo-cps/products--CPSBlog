7d8f6901bbf3dbbcd46d525d009d30c2c8f76a0b
rspivak <devnull@localhost>
2004-12-16 16:08:07 +0000

    Changed algorithm for searching .cps_discussions discussion folder for getting number of comments to proxy.

--- skins/cpsblog/getNumberOfComments.py
+++ skins/cpsblog/getNumberOfComments.py
@@ -1,17 +1,25 @@
-##parameters=proxy_blogentry
-"""Retrieves number of ForumPost objects
-under .cps_discussions directory for the given proxy_blogentry.
-This gives us total number of comments posts to proxy_blogentry.
+##parameters=proxy
+"""Retrieves number of ForumPost objects under .cps_discussions directory
+for the given proxy. This gives us total number of comments posts to proxy.
 """
 
 # $Id$
 
-path = list(context.portal_url.getRelativeContentPath(proxy_blogentry))
-path.insert(-1, '.cps_discussions')
-path = '/'.join(path)
+def getDiscussionsFolder(proxy):
+    getParentNode = lambda node: getattr(getattr(node, 'aq_inner', None),
+                                         'aq_parent', None)
+    parent = getParentNode(proxy)
+    while parent:
+        if hasattr(parent, '.cps_discussions'):
+            return getattr(parent, '.cps_discussions')
+        parent = getParentNode(parent)
+    return None
 
-obj = context.restrictedTraverse(path, None)
-if obj is None:
-    return 0
-else:
-    return len(obj.objectIds())
+discussions_folder = getDiscussionsFolder(proxy)
+
+if discussions_folder is not None:
+    forum = getattr(discussions_folder, proxy.getId(), None)
+    if forum is not None:
+        return len(forum.objectIds())
+
+return 0
