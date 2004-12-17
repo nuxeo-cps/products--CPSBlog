a2bd91319a267ceea72b119a6da4dfc04f22e71c
rspivak <devnull@localhost>
2004-12-17 10:29:21 +0000

    Computes the correct list of URL parameters for blogcalendar_cpsday_view

---
+++ skins/cpsblog/getBlogCalCPSDayViewParams.py
@@ -0,0 +1,15 @@
+##parameters=context_url,datestring,location,event_types
+# $Id$
+"""Compute the correct list of URL parameters for blogcalendar_cpsday_view"""
+
+from urllib import urlencode
+
+args = {'date': datestring}
+
+if location and not location.isspace():
+    args['location'] = location
+
+if event_types:
+    args['event_types'] = ','.join(event_types)
+
+return context_url + '/blogcalendar_cpsday_view?' + urlencode(args)
