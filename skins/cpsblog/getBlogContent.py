c334678c2aff6a63daf5ed156a01eae213fd61a7
rspivak <devnull@localhost>
2005-01-07 19:56:10 +0000

    Added script that iterates over tree and returns content of first blog proxy it encounters.

---
+++ skins/cpsblog/getBlogContent.py
@@ -0,0 +1,24 @@
+##parameters=proxy=None
+# $Id$
+"""Iterates over tree and returns content of first blog proxy it
+encounters.
+"""
+
+if proxy is None:
+    proxy = context
+
+def isBlogType(proxy):
+    return proxy.portal_type == 'Blog'
+
+# check if current context is of Blog type
+if isBlogType(proxy):
+    return proxy.getContent()
+
+getParentNode = lambda node: getattr(getattr(node, 'aq_inner', None),
+                                     'aq_parent', None)
+parent = getParentNode(proxy)
+while parent:
+    if isBlogType(parent):
+        return parent.getContent()
+    parent = getParentNode(parent)
+    return None
