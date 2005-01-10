c65fdd91b7c743e88e7a985786279db8f6208ede
rspivak <devnull@localhost>
2005-01-10 21:04:21 +0000

    Refactored to remove day_separators stuff into separate script - getBlogEntryDaySeparators

--- skins/cpsblog/getSortedBlogEntries.py
+++ skins/cpsblog/getSortedBlogEntries.py
@@ -1,18 +1,6 @@
 ##parameters=sort_on='created',sort_order='reverse',start_date=None,end_date=None
 # $Id$
-"""Returns tuple containing list of sorted blog entries and day_separators
-list containing ids of blog entry proxies which marks first objects in group
-of objects posted in the same year-month-day.
-Lets say we have resulting brains sorted on creation date in reverse order and
-containing:
-[test5, test4, test3, test2, test1]
-
-test5, test4 are posted on January 4
-test3, test2, test1 are posted on January 3
-then our day_separators will look like:
-[test5, test3]
-day_separators makes sense when search query is sorted on creation date and is
-used for visually groupping blog entries by day."""
+"""Returns list of sorted blog entries."""
 
 catalog = context.portal_catalog
 
@@ -29,18 +17,4 @@ if start_date and end_date:
 
 brains = catalog.searchResults(**query)
 
-day_separators = []
-
-if len(brains) > 0:
-    day_separators.append(brains[0].getObject().getId())
-
-def get_date(ob):
-    return ob.created().strftime('%Y-%m-%d')
-
-for i in range(1, len(brains)):
-    ob = brains[i].getObject()
-    prev_ob = brains[i-1].getObject()
-    if get_date(ob) != get_date(prev_ob):
-        day_separators.append(ob.getId())
-
-return filter(None, [brain.getObject() for brain in brains]), day_separators
+return filter(None, [brain.getObject() for brain in brains])
