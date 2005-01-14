c05903490bee0f48469551ec72d398759c55b00d
rspivak <devnull@localhost>
2005-01-14 02:10:19 +0000

    ATOM export support for Blog and BlogAggregator

---
+++ skins/cpsblog/atom.py
@@ -0,0 +1,137 @@
+##parameters=
+# $Id$
+"""Creates an ATOM feed for BlogAggregator and Blog portal types."""
+
+import re
+from urlparse import urlparse
+
+SUMMARY_MAX_LENGTH = 200
+
+items = []
+if context.portal_type == 'BlogAggregator':
+    items = context.getContent().getSearchResults(context)
+elif context.portal_type == 'Blog':
+    items = context.getSortedBlogEntries()
+
+atom_feed = r"""<?xml version="1.0" encoding="ISO-8859-15"?>
+<feed version="0.3" xmlns="http://purl.org/atom/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/">
+  <title type="text/plain">%(feed_title)s</title>
+  <tagline type="text/plain">%(feed_tagline)s</tagline>
+  <link rel="alternate" type="text/html" href="%(feed_link)s" />
+  <id>%(feed_id)s</id>
+  <generator url="%(generator_url)s" version="%(generator_version)s">CPS</generator>
+  <modified>%(feed_modified)s</modified>
+
+  %(entries)s
+</feed>
+"""
+# " <- for Emacs Python mode, argh
+
+
+atom_entry = r"""
+  <entry>
+    <id>%(entry_id)s</id>
+    <title type="text/plain">%(entry_title)s</title>
+    <link rel="alternate" type="text/html" href="%(entry_link)s" />
+    <issued>%(entry_issued)s</issued>
+    <modified>%(entry_modified)s</modified>
+    <author>
+      <name>%(entry_author)s</name>
+    </author>
+    %(entry_contributors)s
+    <summary xml:lang="%(entry_lang)s">
+      %(summary)s
+    </summary>
+    <content type="application/xhtml+xml" mode="xml" xml:lang="%(entry_lang)s">
+      <div xmlns="http://www.w3.org/1999/xhtml">
+        %(content)s
+      </div>
+    </content>
+  </entry>
+"""
+
+entry_contributor =r"""<contributor>
+      <name>%(contributor)s</name>
+    </contributor>
+"""
+
+def construct_id(permalink, datetime):
+    """<link rel="alternate"> is always the permalink of the entry
+    http://diveintomark.org/archives/2004/05/28/howto-atom-id - article
+    about constructing id
+    """
+    (location, path) = urlparse(permalink)[1:3]
+    i = location.rfind(':')
+    if i > 0:
+        location = location[:i]
+    uid = 'tag:' + location + ',' + datetime.strftime('%Y-%m-%d') + ':' + path
+    return uid
+
+# dublin core available from getContentInfo
+dc_keys = ('subject', 'date', 'creator',
+           'contributor', 'rights', 'language',
+           'coverage', 'relation', 'source')
+
+base_url = context.portal_url()+'/'
+
+entry_contributors = body_text = ''
+for item in items:
+    info = context.getContentInfo(item, level=1)
+    doc = info['doc']
+    entry_title = info.get('title', '')
+    entry_link = base_url + info.get('rpath')
+    # XXX: issued == doc.created()? or issued it's time when it was published?
+    entry_issued = context.getDateStr(doc.created(), fmt='iso8601_long')
+    entry_modified = context.getDateStr(info.get('time'), fmt='iso8601_long')
+    entry_author = info.get('creator')
+    entry_lang = info.get('language')
+    entry_id = construct_id(entry_link, doc.created())
+
+    for contributor in doc.Contributors():
+        entry_contributors += entry_contributor % {'contributor' : contributor}
+
+    def strip_html(text):
+        # stripping of html tags based on simple regexp
+        return re.sub("<[^>]+>", '', text)
+
+    if len(doc.Description()) > 0:
+        summary = strip_html(doc.Description())
+    else:
+        summary = strip_html(doc.content)
+        if len(summary) > SUMMARY_MAX_LENGTH:
+            summary = summary[:SUMMARY_MAX_LENGTH]
+            summary = summary[:summary.rindex(' ')] + '...'
+
+    content = doc.content
+
+    body_text += atom_entry % {'entry_id' : entry_id,
+                               'entry_title' : entry_title,
+                               'entry_link' : entry_link,
+                               'entry_issued' : entry_issued,
+                               'entry_modified' : entry_modified,
+                               'entry_author' : entry_author,
+                               'entry_lang' : entry_lang,
+                               'entry_contributors' : entry_contributors,
+                               'summary' : summary,
+                               'content' : content
+                               }
+
+info = context.getContentInfo(context, level=1)
+feed_title = info.get('title', '')
+feed_link = base_url + info.get('rpath')
+feed_id = construct_id(feed_link, context.created())
+feed_modified = context.getDateStr(context.created(), fmt='iso8601_long')
+generator_url = 'http://nuxeo.com'
+generator_version = '.'.join([str(v) for v in context.cps_version[1:]])
+
+text = atom_feed % {'feed_title' : feed_title,
+                    'feed_tagline' : 'ATOM feed',
+                    'feed_link' : feed_link,
+                    'feed_id' : feed_id,
+                    'generator_url' : generator_url,
+                    'generator_version' : generator_version,
+                    'feed_modified' : feed_modified,
+                    'entries' : body_text
+                    }
+
+return text
