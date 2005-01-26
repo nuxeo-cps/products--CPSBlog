86d77e5678f73ac23e71eb60440f7d072ea3ec22
rspivak <devnull@localhost>
2005-01-26 21:00:59 +0000

    Modified to take categories from global blog categories and from blog itself.

--- skins/cpsblog/getVocBlogCategories.py
+++ skins/cpsblog/getVocBlogCategories.py
@@ -3,14 +3,25 @@
 """Returns blog categories for method vocabulary."""
 
 blog = context.getBlogContent()
+glob_cats_voc = context.portal_vocabularies.blog_glob_categories
 
 if key is not None and is_i18n is not None:
     category = blog.getCategoryByTitle(key)
+    if category is None:
+        return glob_cats_voc.getMsgid(key)
     return category['title']
 elif key is not None:
     category = blog.getCategoryByTitle(key)
+    if category is None:
+        return glob_cats_voc.get(key)
     return category['title']
 
-categories = [(cat['title'], cat['title']) for cat
-              in blog.getSortedCategories()]
+blog_categories = [(cat['title'], cat['title']) for cat
+                   in blog.getSortedCategories()]
+
+glob_categories = [(k, v) for k, v in glob_cats_voc.items()]
+
+categories = blog_categories + glob_categories
+categories.sort()
+
 return tuple(categories)
