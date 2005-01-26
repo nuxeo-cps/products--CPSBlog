ee388327714932c1892e58ca06ae46045905c921
rspivak <devnull@localhost>
2005-01-26 13:19:16 +0000

    Modified to make navigation links be shortened to URL_TITLE_MAX_LENGTH and append ellipses in case url title is too long.

--- skins/cpsblog/getBlogEntryNavLinksInfo.py
+++ skins/cpsblog/getBlogEntryNavLinksInfo.py
@@ -3,6 +3,18 @@
 """Returns info dictionary containing urls and titles of previous and next
 blog entry with regard to current blog entry."""
 
+URL_TITLE_MAX_LENGTH = 40
+
+def strip_title(title):
+    if len(title) > URL_TITLE_MAX_LENGTH:
+        stitle = title[:URL_TITLE_MAX_LENGTH]
+        i = stitle.rfind(' ')
+        if i > 0:
+            stitle = stitle[:i]
+        stitle += '...'
+        return stitle
+    return title
+
 proxy = context.getBlogProxy()
 bentries = proxy.getSortedBlogEntries(sort_order='ascending')
 
@@ -16,12 +28,12 @@ for i in range(blen):
             info['next_title'] = ''
         else:
             info['next_url'] = bentries[i+1].absolute_url()
-            info['next_title'] = bentries[i+1].Title()
+            info['next_title'] = strip_title(bentries[i+1].Title())
         if i - 1 < 0:
             info['prev_url'] = ''
             info['prev_title'] = ''
         else:
             info['prev_url'] = bentries[i-1].absolute_url()
-            info['prev_title'] = bentries[i-1].Title()
+            info['prev_title'] = strip_title(bentries[i-1].Title())
         return info
 return None
