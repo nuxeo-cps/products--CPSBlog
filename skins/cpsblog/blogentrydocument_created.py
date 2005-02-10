d066e4d20addcdcbbc616c35cb6ad0a3161326af
rspivak <devnull@localhost>
2005-02-10 21:48:29 +0000

    Added post creation actions for blog entry: adding and sending dispatch trackbacks

--- skins/cpsblog/blogentrydocument_created.py
+++ skins/cpsblog/blogentrydocument_created.py
@@ -10,6 +10,9 @@ May return a rendered document, or do a redirect.
 # $Id$
 
 psm = 'psm_content_created'
+
+context.getContent().addDispatchTrackbacks(context=context)
+
 action_path = context.getTypeInfo().immediate_view
 context.REQUEST.RESPONSE.redirect('%s/%s?portal_status_message=%s' %
                                   (context.absolute_url(), action_path, psm))
