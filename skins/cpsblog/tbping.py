630e4cdfee3187af04912e9438eb3a2aca23951f
rspivak <devnull@localhost>
2005-02-17 15:52:50 +0000

    Removed unneeded script as tbping method of BlogEntry no more requires it to work properly and will get context from REQUEST.

--- skins/cpsblog/tbping.py
+++
@@ -1,7 +0,0 @@
-##parameters=REQUEST=None
-# $Id$
-"""Passes context and REQUEST to the real object's tbping method
-for handling Trackback's GET and POST request and returns result.
-Context of the script should always be blog entry."""
-
-return context.getContent().tbping(context, REQUEST)
