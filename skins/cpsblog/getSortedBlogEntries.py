809fcf161b806c79dbf96316a667467772fafce5
rspivak <devnull@localhost>
2005-02-21 14:07:21 +0000

    - Added 'path' parameter to script, if omitted, then path will be path to context. - Added 'cps_filter_sets' to search query to select only searchable blog entries

--- skins/cpsblog/getSortedBlogEntries.py
+++ skins/cpsblog/getSortedBlogEntries.py
@@ -1,6 +1,8 @@
-##parameters=sort_on='created',sort_order='reverse',start_date=None,end_date=None,category=None
+##parameters=sort_on='created',sort_order='reverse',start_date=None,end_date=None,category=None,path=None
 # $Id$
-"""Returns list of sorted blog entries."""
+"""Returns list of sorted blog entries.
+If path parameter is None then path will be computed as path to context.
+"""
 
 catalog = context.portal_catalog
 
@@ -8,7 +10,11 @@ query = {}
 query['meta_type'] = 'BlogEntry'
 query['sort_on'] = sort_on
 query['sort_order'] = sort_order
-query['path'] = '/'.join(context.getPhysicalPath())
+query['cps_filter_sets'] = 'searchable'
+if path is not None:
+    query['path'] = path
+else:
+    query['path'] = '/'.join(context.getPhysicalPath())
 
 if start_date and end_date:
     from DateTime import DateTime
