6c54d733e5588eebef9247ca3a970a9c2c021c53
rspivak <devnull@localhost>
2005-02-22 19:02:43 +0000

    Changed the algo how categories, that will be displayed in blog categories box/portlet, will be computed.

--- skins/cpsblog/getCPSBlogCategories.py
+++ skins/cpsblog/getCPSBlogCategories.py
@@ -5,8 +5,8 @@ posts within that category and boolean determining if category is global.
 
 For blog aggregator: categories are all categories referenced in blog
 entries displayed for this blog aggregator.
-For blog: categories are union of global categories and local categories
-of this blog."""
+For blog: categories are categories that are present in blog entries
+of current blog, they may be global or/and local"""
 
 categories = {}
 brains = []
@@ -17,21 +17,7 @@ if proxy.portal_type == 'BlogAggregator':
     # entries displayed for this blog aggregator
     entries = proxy.getContent().getSearchResults(proxy)
 elif proxy.portal_type == 'Blog':
-    # for blog, categories are union of global categories and local categories
-    # of this blog
-    catalog = context.portal_catalog
-    query = {}
-    query['portal_type'] = 'BlogEntry'
-    query['Subject'] = {'query' : glob_categories, 'operator' : 'or'}
-    query['cps_filter_sets'] = 'searchable'
-    brains = catalog.searchResults(**query)
-    items = filter(None, [brain.getObject() for brain in brains])
-    items.extend(proxy.getSortedBlogEntries())
-    unique_entries = {}
-    # filter out entries that are present in global search result
-    for entry in items:
-        unique_entries[entry.absolute_url()] = entry
-    entries = unique_entries.values()
+    entries = proxy.getSortedBlogEntries()
 
 for entry in entries:
     for category in entry.getContent().Subject():
