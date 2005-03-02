b31c761de6f984a84538276fdb2ae139b968cef5
rspivak <devnull@localhost>
2005-03-02 14:19:56 +0000

    Reindex 'start' and 'end' when publishing blog entry to make calendar happy.

--- Extensions/install.py
+++ Extensions/install.py
@@ -365,7 +365,7 @@ now = DateTime()
 blog_entry_proxy = state_change.object
 blog_entry_proxy.getEditableContent().setEffectiveDate(now)
 blog_entry_proxy.setEffectiveDate(now)
-blog_entry_proxy.reindexObject(idxs=['effective'])
+blog_entry_proxy.reindexObject(idxs=['effective', 'start', 'end'])
 """
             },
             }
