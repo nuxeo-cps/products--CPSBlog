53abcd96d9a0fb8a394ae2907eff34aa35eacdc9
rspivak <devnull@localhost>
2005-08-13 18:55:21 +0000

    Added upgrade file with method to upgrade BlogAggregator: search_limit field in schema

---
+++ Extensions/upgrade.py
@@ -0,0 +1,35 @@
+# (C) Copyright 2004-2005 Nuxeo SARL <http://nuxeo.com>
+# Author: Ruslan Spivak <rspivak@nuxeo.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+#
+# $Id$
+
+
+def update_blogaggregator(self):
+    """Updates BlogAggregator's schema.
+
+    Adds search_limit field is not present
+    """
+    catalog = self.portal_catalog
+
+    proxies = [b.getObject()
+               for b in catalog.searchResults(portal_type='BlogAggregator')]
+
+    for proxy in proxies:
+        if getattr(proxy, 'search_limit', None) is None:
+            doc = proxy.getContent()
+            doc.search_limit = 20
+    return 'Fin'
