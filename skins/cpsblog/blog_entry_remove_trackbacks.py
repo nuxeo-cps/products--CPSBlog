46cab4cb517c4df8dc75457a3fc6454a5c015702
rspivak <devnull@localhost>
2005-08-13 21:39:17 +0000

    Modified to make trackbacks deletion work with latest CPS, maybe backward incompatible. needs reviewing more.

--- skins/cpsblog/blog_entry_remove_trackbacks.py
+++ skins/cpsblog/blog_entry_remove_trackbacks.py
@@ -4,6 +4,9 @@
 layout for blog entry, so 'context' is repository blog entry object,
 not proxy."""
 
+if getattr(context, 'removeTrackbacks', None) is None:
+    context = context.getContent()
+
 context.removeTrackbacks(trackbacks_ids)
 
 REQUEST.RESPONSE.redirect(REQUEST['HTTP_REFERER'])
