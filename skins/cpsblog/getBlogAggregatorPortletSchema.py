bf52c90d75ca4ffaba85a31f16e2f212ed921606
jmorliaguet <devnull@localhost>
2005-08-16 12:16:35 +0000

    - removed the 'custom_cache_params' schema field set to 'no-cache'.   This is meant to be used with Custom Portlets only.   Also this is redundant: if no cache parameter is found for a given portlet   then 'no-cache' will be used by default.   Also setting the parameter here make it impossible to set other cache   parameters since 'no-cache' will be appended and override all other   parameters.   To update cache parameters, the portal_cpsportlets updateCacheParameters()   should be used instead. (see also the 'Cache parameters' tab in the ZMI)
    NOTE: existing blog aggregator portlets still have the 'custom_cache_params'
     field set, so they will never be cached unless they are recreated.

--- skins/cpsblog/getBlogAggregatorPortletSchema.py
+++ skins/cpsblog/getBlogAggregatorPortletSchema.py
@@ -17,24 +17,6 @@
 # $Id$
 
 blogaggregator_portlet_schema = {
-    'custom_cache_params': {
-        'type': 'CPS String List Field',
-        'data': {
-            'default_expr': "python:['no-cache']",
-            'is_searchabletext': False,
-            'acl_read_permissions': '',
-            'acl_read_roles': '',
-            'acl_read_expr': '',
-            'acl_write_permissions': '',
-            'acl_write_roles': '',
-            'acl_write_expr': '',
-            'read_ignore_storage': False,
-            'read_process_expr': '',
-            'read_process_dependent_fields': (),
-            'write_ignore_storage': False,
-            'write_process_expr': '',
-            },
-        },
     'query_title': {
         'type': 'CPS String Field',
         'data': {'default_expr': 'string:',
