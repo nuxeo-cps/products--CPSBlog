5edc2a381313d77748066408b32498c8aff47125
jmorliaguet <devnull@localhost>
2005-08-19 14:57:55 +0000

    - added a FIXME comment when exporting aggregators
      (cf http://blogs.nuxeo.com/sections/aggregators/all_posts/exportatom)

--- skins/cpsblog/exportatom.py
+++ skins/cpsblog/exportatom.py
@@ -2,6 +2,8 @@
 # $Id: exportatom.py 25893 2005-08-16 22:55:23Z sfermigier $
 """Creates an ATOM feed for BlogAggregator and Blog portal types."""
 
+# FIXME: 'result' is accessed before assignment if the portal_type is BlogAggregator
+
 if context.portal_type == 'Blog':
     result = context.atomFeed()
 elif context.portal_type == 'BlogEntry':
